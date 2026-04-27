c7e6a1cf160ad306e5461fab151b70f8cb311c2f
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-04-27 13:50:25 -0600

    Add files via upload

---
+++ Guia4_TiffanyGodoy/src/main/java/com/mycompany/guia4_tiffanygodoy/Guia4_TiffanyGodoy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guia4_tiffanygodoy;
+
+/**
+ *
+ * @author LAB-D-PC12
+ */
+public class Guia4_TiffanyGodoy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
